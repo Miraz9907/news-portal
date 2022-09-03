c6c9c880d858c9f5fda8af3e0c8ec2219392b091
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-03 15:36:46 +0600

    news section updated

--- js/news.js
+++ js/news.js
@@ -28,6 +28,84 @@ const displayCategories = (category) => {
 loadCategories();
 
 
+const displayNewsCategory = async(category_id,category_name) =>{
+  // toggleSpinner(true);
+  console.log(category_id);
+  const url=  `https://openapi.programming-hero.com/api/news/category/${'0'+category_id}`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayNews(data.data);
+  const messageField = document.getElementById('message-feild');
+  const messageFieldValue = `${data.data.length} items found for this category ${category_name}` ;
+  messageField.value = messageFieldValue;
+
+  
+}
+const displayNews = (data) =>{
+  
+  // sort by view
+  data.sort((a, b) => b.total_view- a.total_view);
+
+
+  console.log(data);
+  const messageFieldContainer = document.getElementById('news-container');
+  messageFieldContainer.textContent= '';
+
+  data.forEach(news =>{
+      const newsSection = document.createElement('div');
+      newsSection.classList.add('row');
+      newsSection.classList.add('mb-4');
+      newsSection.classList.add('bg-light');
+     
+      
+      console.log(news);
+      newsSection.innerHTML=`
+      <div class="col-lg-4">
+          <img src="${news.image_url}" alt="" class="img-fluid h-100 p-3 rounded ">
+      </div>
+      <div class="col-md-8 p-5 ">
+          <h3>${news.title}</h3>
+          <p>${news.details.length > 506 ? news.details.slice(0,506) +'...' : news.details }</p>
+
+              <div class="d-flex align-items-center justify-content-between">
+                  <div class="d-flex align-items-center ">
+                      <img src="${news.author.img}" alt="" height="30" class="rounded-circle">
+                      <div >
+                          <p>${news.author.name} </p>
+                          <p>${news.author.published_date}</p>
+                      </div>
+                  </div>
+
+                  <div>
+                      <span><i class="fa-sharp fa-solid fa-eye"></i> ${news.total_view}M</span>
+
+                  </div>
+
+                  <div>
+                      <i class="fa-solid fa-star-half-stroke"></i>
+                      <i class="fa-regular fa-star"></i>
+                      <i class="fa-regular fa-star"></i>
+                      <i class="fa-regular fa-star"></i>
+                      <i class="fa-regular fa-star"></i>
+                  </div>
+
+                  <div>
+                      <i class="fa-solid fa-arrow-right btn" data-bs-toggle="modal" data-bs-target="#phoneDetailModal"></i>
+                  </div>
+              </div>
+              
+      </div>
+  
+      `;
+      messageFieldContainer.appendChild(newsSection);
+
+  });
+
+}
+
+
+
+
 
 
 
